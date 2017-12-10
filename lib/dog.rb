6c589ffbf98e4b1cfe7a5acefb9a35faa4365e9a
tal shachar <tal.shachar@buzzfeed.com>
2017-12-10 00:21:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   end
 
   def name
-    puts "@name"
+    @name
   end
 
   def breed=(breed)
@@ -13,7 +13,7 @@ class Dog
   end
 
   def breed
-    puts "@breed"
+    @breed
   end
 
 end
